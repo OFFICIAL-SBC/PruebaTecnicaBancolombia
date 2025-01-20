1604cf9a8306e486fb3882787da3b1f661517fab
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2025-01-20 12:11:30 -0500

    Backend terminado.

--- Backend/src/Controllers/country.controller.js
+++ Backend/src/Controllers/country.controller.js
@@ -1,17 +1,35 @@
 const localStorage = require('../localstorage');
 
 const getCountryCoins = (req, res) => {
-
-    console.log(req);
+    
     const countryName = req.params.countryname;
     const countryFound = localStorage.get('countries').find((c) => c.name === countryName);
-    const currencies = localStorage.get('currencies').filter((c) => countryFound.allowedCurrencies.includes(c.id));
 
-    if (!currencies) {
+    if (!countryFound) {
+        return res.status(404).json({ error: 'Country not found' });
+    }
+
+    const currenciesWithManagers = countryFound.allowedCurrencies.reduce((acc, currencyId) => {
+        const manager = countryFound.managers.find(manager => manager.associatedCurrencies.includes(currencyId));
+        acc[currencyId] = manager ? manager.name : "Manager no asignado";
+        return acc;
+    }, {});
+
+    const currencies = localStorage.get('currencies')
+        .filter((c) => countryFound.allowedCurrencies.includes(c.id))
+        .map((c) => ({
+            id: c.id,
+            name: c.name,
+            symbol: c.symbol,
+            valueInDollars: c.valueInDollars,
+            manager: currenciesWithManagers[c.id]
+        }));
+
+    if (currencies.length === 0) {
         return res.status(404).json({ error: 'Currencies not found' });
     }
 
     res.json({ coins: currencies });
 };
 
-module.exports = { getCountryCoins};
+module.exports = { getCountryCoins };
--- Backend/src/localstorage.js
+++ Backend/src/localstorage.js
@@ -1,24 +1,29 @@
 let storage = {
   users: [
-    { id: 1, name: 'admin', countryId: 1, associatedCurrencies: [1, 2] },
-    { id: 2, name: 'user', countryId: 2, associatedCurrencies: [3] },
+    { id: 1, name: 'sebastian', countryId: 1, associatedCurrencies: [1, 2] },
+    { id: 2, name: 'pablo', countryId: 2, associatedCurrencies: [3] },
   ],
   countries: [
     {
       id: 1,
       name: 'USA',
-      allowedCurrencies: [1, 2, 5],
-      managers: ['Manager1', 'Manager2'], // Gestoras activas
+      allowedCurrencies: [1, 2],
+      managers: [
+        { name: 'TechSol SAS', associatedCurrencies: [1] },
+        { name: 'TREE SAS', associatedCurrencies: [2] }
+      ], 
     },
     {
       id: 2,
       name: 'Colombia',
       allowedCurrencies: [3],
-      managers: ['Manager3'], // Gestoras activas
+      managers: [
+        { name: 'Bancolombia', associatedCurrencies: [3] }
+      ], 
     },
   ],
   currencies: [
-    { id: 1, name: 'Bitcoin', symbol: 'BTC', valueInDollars: 42000 },
+    { id: 1, name: 'Bitcoin', symbol: 'BTC', valueInDollars: 42000},
     { id: 2, name: 'Ethereum', symbol: 'ETH', valueInDollars: 3200 },
     { id: 3, name: 'Solana', symbol: 'SOL', valueInDollars: 20 },
     { id: 4, name: 'CrytoPeso', symbol: 'COL', valueInDollars: 200 },
